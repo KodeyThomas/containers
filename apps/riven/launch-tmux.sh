a4b51c713a168ac2825bc76d1b34087a145ee65a
Kodey Kilday-Thomas <kodey@kodey.dev>
2024-11-12 15:54:49 +0000

    feat(riven): inital release

---
+++ apps/riven/launch-tmux.sh
@@ -0,0 +1,3 @@
+#!/bin/ash
+
+tmux -f /riven/restricted.tmux.conf new-session -A -s riven /riven/riven.sh
---
+++ apps/riven/riven.sh
@@ -0,0 +1,37 @@
+#!/bin/ash
+
+cd /riven/src
+
+cd /riven/src
+
+echo "💥 For the option to reset your database, press the 'x' key within 10 seconds.."
+
+# Read a single character with a timeout of 10 seconds
+read -n 1 -t 10 key
+clear # clear after user pressed Cancel
+
+# Check if the key pressed is 'x'
+if [[ "$key" == "x" ]]; then
+    read -p "Wipe Riven's database first (y/n)?" choice
+    case "$choice" in 
+    y|Y ) 
+        python main.py --hard_reset_db
+        # workaround
+        PGPASSWORD=postgres psql -U postgres -d riven -h localhost -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+        rm -rf /riven/alembic
+        ;;
+    * ) ;; # do nothing
+    esac
+    clear # clear after user pressed Cancel
+elif [[ "$key" == "z" ]]; then
+    bash # drop to a shell
+fi
+
+#echo "📺 Waiting for plex to be up..."
+#/usr/local/bin/wait-for -t 3600 plex:32400 -- echo "✅"
+
+echo "👽 Waiting for zurg to be up in ${ZURG_URL:-"zurg.zurg.svc.cluster.local:9999"}..."
+/usr/local/bin/wait-for -t 3600 ${ZURG_URL:-"zurg.zurg.svc.cluster.local:9999"} -- echo "✅"
+
+echo "🎉 let's go!"
+poetry run python3 main.py 
