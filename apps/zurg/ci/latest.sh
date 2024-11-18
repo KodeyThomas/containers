351c22a7b296ef08156b84d229596cf7409cc2f0
Kodey Kilday-Thomas <kodey@kodey.dev>
2024-11-18 07:34:28 +0000

    feat(zurg): initial release

---
+++ apps/zurg/ci/latest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+channel=$1
+version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg/releases --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | first | .tag_name')
+printf "%s" "${version}"
