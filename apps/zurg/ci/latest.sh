a199742b516a80472a72e8ac15b33ad2af64cda3
Kodey Kilday-Thomas <kodey@kodey.dev>
2024-11-18 09:42:02 +0000

    fix(zurg): use ssh

--- apps/zurg/ci/latest.sh
+++ apps/zurg/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 channel=$1
-version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg/releases --header "Authorization: Bearer ${ZURG_GH_CREDS}" | jq --raw-output '. | first | .tag_name')
+version=$(curl -sX GET https://api.github.com/repos/debridmediamanager/zurg/releases --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '. | first | .tag_name')
 printf "%s" "${version}"
