5c50663046cb8d47a40e2140db6a36b955c92e01
Kodey Kilday-Thomas <kodey@kodey.dev>
2024-11-24 09:19:16 +0000

    feat(rclone): initial release

---
+++ apps/rclone/rootfs/webdav.sh
@@ -0,0 +1,31 @@
+#!/bin/ash
+
+cd /storage
+
+if [[ ! -z "${WEBDAV_PASSWORD}" ]]; then
+/usr/bin/rclone serve webdav \
+    --addr :5574 \
+    --user ${WEBDAV_USERNAME} \
+    --pass "${WEBDAV_PASSWORD}" \
+    --max-header-bytes=8192 \
+    --vfs-read-chunk-size=10M \
+    --vfs-read-chunk-size-limit=10M \
+    --stats 60s \
+    --copy-links \
+    -v \
+    /storage
+else
+    mkdir /tmp/no-password
+    echo "WebDAV password not set!" > /tmp/no-password/README.txt
+    /usr/bin/rclone serve webdav \
+    --addr :5574 \
+    --user ${WEBDAV_USERNAME} \
+    --pass "${WEBDAV_PASSWORD}" \
+    --max-header-bytes=8192 \
+    --vfs-read-chunk-size=10M \
+    --vfs-read-chunk-size-limit=10M \
+    --stats 60s \
+    --copy-links \
+    -v \
+    /tmp/no-password    
+fi
