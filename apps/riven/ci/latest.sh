d2cf13cb6b45ff48cd8276447019ac9e032004e3
Kodey Kilday-Thomas <kodey@kodey.dev>
2024-11-25 08:47:30 +0000

    feat(riven): add dev channel

--- apps/riven/ci/latest.sh
+++ apps/riven/ci/latest.sh
@@ -3,7 +3,6 @@
 channel=$1
 if [[ "${channel}" == "dev" ]]; then
     version="feature/torbox"
-	printf "%s" "${version}"
 else
 	version="$(curl -sX GET "https://api.github.com/repos/rivenmedia/riven/releases/latest" --header "Authorization: Bearer ${TOKEN}" | jq --raw-output '.tag_name')"
 fi
